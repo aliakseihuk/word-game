a4439767681cbef1f6793334c35e3093a15e176d
Aliaksei Huk <alexeyguk@gmail.com>
2018-01-17 01:23:39 +0300

    fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import configureStore, { history } from './store/configureStore';
 import { App } from './App';
+
+const store = configureStore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>,
+    div
+  );
 });
