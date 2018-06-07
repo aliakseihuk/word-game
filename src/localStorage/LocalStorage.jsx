2b07eba9648e74cfa2c36be91743a0998c591602
Roman <r.dyro@itransition.com>
2018-06-07 16:11:42 +0300

    Add LocalStorage module

---
+++ src/localStorage/LocalStorage.jsx
@@ -0,0 +1,34 @@
+class LocalStorage {
+  static isEmpty() {
+    return !localStorage.length;
+  }
+
+  static saveData(name, data) {
+    localStorage.setItem(name, this.prepareSaveData(JSON.stringify(data)));
+  }
+
+  static readData(name) {
+    const data = localStorage.getItem(name);
+    return JSON.parse(this.prepareLoadData(data));
+  }
+
+  static hydrateComponentState(component) {
+    for (let key in component.state) {
+      const data = this.readData(key);
+
+      if (data) {
+        component.setState({ [key]: data });
+      }
+    }
+  }
+
+  static prepareSaveData(data) {
+    return btoa(data);
+  }
+
+  static prepareLoadData(data) {
+    return atob(data);
+  }
+}
+
+export default LocalStorage;
