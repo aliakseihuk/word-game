a57c48a898bf4319ddb11e1708f6ba9f7e3e96d6
LoserAntbear <tollemet@gmail.com>
2018-12-26 00:36:36 +0300

    Move <Word> Component to typescript

---
+++ src/components/Word/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Letter from 'src/components/Letter';
+import { FILL_PATTERNS } from 'src/constants';
+
+import './Word.css';
+
+type Props = {
+  letters: string[],
+  length: number,
+};
+
+const Word: React.SFC<Props> = ({ letters, length }): JSX.Element => {
+  const pattern = FILL_PATTERNS.DASH;
+  const tail = Array(length - letters.length).fill(pattern);
+  const renderedLetters = letters.concat(tail);
+  const elements = renderedLetters.map((letter, i) => (
+    <Letter value={letter} isPlaceholder={i >= letters.length} key={i + letter} />
+  ));
+  
+  return <section className="word">{elements}</section>;
+};
+
+export default Word;
